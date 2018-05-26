1e87b7038fdc2f06b25a4f4fa688dec386dee17d
m-bilalqayoom <bilalchhipa1234@gmail.com>
2018-05-26 20:06:53 +0500

    1st commit
    it includes html css and javascript

---
+++ librarymange/app.js
@@ -0,0 +1,37 @@
+
+var description = function(){
+var ui = document.getElementById("uid").value;
+console.log("User Id"+ui);
+
+var unam = document.getElementById("uname").value;
+console.log("User name "+unam);
+
+var fnam = document.getElementById("fname").value;
+console.log("father name"+fnam);
+
+var clastu = document.getElementById("classstu").value;
+console.log("Class of student"+clastu);
+
+
+
+}
+function select1() {
+    var u = document.getElementById("book array1").value;
+    // document.getElementById("demo").innerHTML =  + x + " Selected";
+console.log("physics "+u);
+}
+function select2() {
+    var x = document.getElementById("book array1").value;
+    // document.getElementById("demo").innerHTML =  + x + " Selected";
+console.log("Chemistry"+x);
+}
+function select3() {
+    var y = document.getElementById("book array1").value;
+    // document.getElementById("demo").innerHTML =  + x + " Selected";
+console.log("Botany"+y);
+}
+function select4() {
+    var z= document.getElementById("book array1").value;
+    // document.getElementById("demo").innerHTML =  + x + " Selected";
+console.log("Zology"+z);
+}
